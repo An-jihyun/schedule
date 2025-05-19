ba4903af491fc903a7dfc70bbbd4ad421aac2018
An-jihyun <anjh322@gmail.com>
2025-05-19 11:06:54 +0900

    test

---
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -0,0 +1,34 @@
+package com.example.memo.controller;
+
+import com.example.memo.dto.MemoRequestDto;
+import com.example.memo.dto.MemoResponseDto;
+import com.example.memo.entity.Memo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/memos") // prefix
+public class MemoController {
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
+        // 식별자가 1씩 증가 하도록 만듦
+        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+
+        // 요청받은 데이터로 Memo 객체 생성
+        Memo memo = new Memo(memoId, dto.getTitle(), dto.getContents());
+
+        // Inmemory DB에 Memo 저장
+        memoList.put(memoId, memo);
+
+        return new MemoResponseDto(memo);
+        //123
+    }
+}
